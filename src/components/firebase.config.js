b65e5c3cb74eef26ed56be6c0ca4072c8d7598c5
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-04-06 00:02:43 +0600

    first-time

---
+++ src/components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiblWTKAKJwt-Qoj3Y4JdUgcfITNw8mys",
+    authDomain: "ajker-bazar-mern.firebaseapp.com",
+    projectId: "ajker-bazar-mern",
+    storageBucket: "ajker-bazar-mern.appspot.com",
+    messagingSenderId: "38483806276",
+    appId: "1:38483806276:web:7f5a615f125f8a76af5411"
+};
+export default firebaseConfig;
